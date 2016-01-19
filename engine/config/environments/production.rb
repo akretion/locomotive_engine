fe3135df7f3213162c87063fc698c75a632e76d5
Gordon Burgett <gordon.burgett@gmail.com>
2016-01-19 19:34:41 +0100

    added mailgun settings

--- engine/config/environments/production.rb
+++ engine/config/environments/production.rb
@@ -60,7 +60,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailgun
+  config.action_mailer.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => mg.gordonburgett.net,
+    :user_name      => postmaster@mg.gordonburgett.net,
+    :password       => ENV['MG_PASSWORD'],
+    :authentication => :plain,
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
